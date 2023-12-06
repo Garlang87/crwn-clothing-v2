7ce1bb997362574de53a2610a41c995c7bfc4354
unknown <jaime_sg@hotmail.com>
2023-12-05 18:33:35 -0600

    Added the functionality to add items to the cart, with updating the counter of the bag, and the style for the dropdown item

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,16 @@
+import { useContext } from 'react'
+import { CartContex } from '../../context/cart.context'
+
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
 import './cart-dropdown.styles.scss'
 
 const CartDropDown = () => {
+    const {cartItems} =  useContext(CartContex);
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                
+                { cartItems.map((item) => (<CartItem key={item.id} cartItem={item}/>))}
             </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react';
+import { CartContex } from '../../context/cart.context';
 import Button from '../button/button.component'
+
 import './product-card.styles.scss'
 
 
 const ProductCard = ({product}) => {
     const {name, price, imageUrl} = product;
+    const {addItemToCart} = useContext(CartContex);
+    const addProductToCart = () => addItemToCart(product);
+
+    
     return(<div className='product-card-container'>
         <img src={imageUrl} alt={`${name}`}></img>
         <div className='footer'>
@@ -11,7 +18,7 @@ const ProductCard = ({product}) => {
             <span className='price'>{price}</span> 
             
         </div>
-        <Button buttonType="inverted">Add to cart</Button>
+        <Button buttonType="inverted" onClick={addProductToCart}>Add to cart</Button>
     </div>)
 }
 
